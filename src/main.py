373e5e10b442b155ab524d20f4a7ad7b72006576
JoaoFrancisc0 <joaofrancisco1015@gmail.com>
2026-02-23 13:10:09 -0300

    feat: add storage_data() in main.py calling the database methods

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-from scraper.engine import extrair_dados # Sua função com Playwright
-from database import save_to_mongo
+from scraper.engine import extrair_dados
+from utils.helpers import comparar_preco
+from database import upsert_collection1, insert_collection2, upsert_collection3, read_collection3
 import logging
+
+def storage_data(dados):
+    latest_prices = read_collection3()
+    products_with_new_prices = comparar_preco(dados, latest_prices)
+    upsert_collection1(products_with_new_prices)
+    insert_collection2(products_with_new_prices)
+    upsert_collection3(products_with_new_prices)
+
 
 def run():
     logging.info("Iniciando o processo de scraping...")
@@ -10,9 +19,12 @@ def run():
     
     # 2. Se houver dados, envia para o MongoDB
     if dados:
-        save_to_mongo(dados)
+      storage_data(dados)
     else:
         logging.warning("Nenhum dado foi coletado para salvar.")
 
 if __name__ == "__main__":
+    import time
+    inicio = time.time()
     run()
+    print(f"Demorou: {round(time.time() - inicio, 2)} segundos")
