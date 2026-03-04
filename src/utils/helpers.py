b9a8357fdf338a99e6a165e9d5f317ecb2c48136
JoaoFrancisc0 <joaofrancisco1015@gmail.com>
2026-03-04 13:22:44 -0300

    feat: improve new way to parse the products name

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -3,29 +3,62 @@ from datetime import datetime
 from utils.constants import *
 
 def normalizar(texto):
-    '''a-z, 0-9, espaço, -'''
+    '''a-z, 0-9, espaço'''
     texto = texto.lower()
     texto = unicodedata.normalize("NFD", texto)
     texto = "".join(c for c in texto if unicodedata.category(c) != "Mn")
-    texto = re.sub(r"[^a-z0-9\s-]", "", texto)
+    texto = re.sub(r"[^a-z0-9\s]", "", texto)
+    texto = re.sub(r"\s+", " ", texto) # une multiplos espaços
     return texto
 
+def remover_expressoes(texto):
+    for exp in REMOVER_EXPRESSOES:
+        padrao = rf"\b{re.escape(exp)}\b"
+        texto = re.sub(padrao, "", texto)
+    return re.sub(r"\s+", " ", texto).strip()
+
+def remover_palavras(texto):
+    palavras = texto.split()
+    palavras_filtradas = [
+        p for p in palavras if p not in REMOVER_PALAVRAS
+    ]
+    texto_limpo = " ".join(palavras_filtradas)
+    return re.sub(r"\s+", " ", texto_limpo).strip()
+
+def remover_palavras_borda(texto):
+    palavras = texto.split()
+
+    while palavras and palavras[0] in REMOVER_PALAVRAS_BORDA:
+        palavras.pop(0)
+        
+    while palavras and palavras[-1] in REMOVER_PALAVRAS_BORDA:
+        palavras.pop()
+        
+    return " ".join(palavras)
+
+def contem_blacklist_expressoes(texto):
+    for exp in BLACKLIST_EXPRESSOES:
+        padrao = rf"\b{re.escape(exp)}\b"
+        if re.search(padrao, texto):
+            return True
+    return False
+
+def contem_blacklist_palavras(texto):
+    palavras = texto.split()
+    return any(p in BLACKLIST_PALAVRAS for p in palavras)
+
+def contem_blacklist(texto):
+    return (
+        contem_blacklist_expressoes(texto)
+        or contem_blacklist_palavras(texto)
+    )
+
 def tratar_nome(texto_nome):
-    nome_tratado = normalizar(texto_nome)
-    
-    for v in FILTRO:
-        padrao = r"\b" + re.escape(v) + r"\b"
-        nome_tratado = re.sub(padrao, "", nome_tratado)
-
-    nome_tratado = " ".join(nome_tratado.split())
-
-    if nome_tratado.endswith("-"):
-        nome_tratado = nome_tratado[:-1].strip()
-
-    for palavra in BLACK_LIST:
-        if palavra in nome_tratado:
-            return None
-
+    nome_normalizado = normalizar(texto_nome)
+    if contem_blacklist(nome_normalizado): return None
+    nome_sem_expressoes = remover_expressoes(nome_normalizado)
+    nome_filtrado = remover_palavras(nome_sem_expressoes)
+    nome_tratado = remover_palavras_borda(nome_filtrado)
     return nome_tratado
 
 def tratar_plataforma(texto_nome, texto_plataforma="Sem Sistema Operacional"):
