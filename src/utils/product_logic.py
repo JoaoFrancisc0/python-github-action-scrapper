466867be08c8495fe7510be75f0de456137cc790
JoaoFrancisc0 <joaofrancisco1015@gmail.com>
2026-03-12 12:18:32 -0300

    fix: now comparar_preco() compars the new_data it self for duplicates

--- src/utils/product_logic.py
+++ src/utils/product_logic.py
@@ -10,17 +10,27 @@ def remover_duplicatas(produtos):
     return list(menores.values())
 
 def comparar_preco(new_data, latest_prices):
+    menor_preco_por_key = {}
+
+    for product in new_data:
+        key = product["key"]
+        preco = product["preco"]
+
+        if key not in menor_preco_por_key or preco < menor_preco_por_key[key]["preco"]:
+            menor_preco_por_key[key] = product
+
+    new_data_filtrado = list(menor_preco_por_key.values())
+
     indice_antigo = {product["key"]: product["preco"] for product in latest_prices}
 
     alterados = []
 
-    for product in new_data:
+    for product in new_data_filtrado:
         key = product["key"]
         preco_novo = product["preco"]
 
         if key not in indice_antigo:
             alterados.append(product)
-
         else:
             preco_antigo = indice_antigo[key]
             if preco_antigo != preco_novo:
